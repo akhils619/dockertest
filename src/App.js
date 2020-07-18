0e74ee18678431d3fef5269b353141a4ef3e8d1e
Akhil <akhils619@gmail.com>
2020-07-18 19:51:49 -0400

    Updated app file

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useLocation } from "./useLocation";
 function App() {
-  const location = useLocation();
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
-    });
-  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Location</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Latitude: {location.latitude}, Latitude: {location.longitude}
+          Learn React
         </a>
       </header>
     </div>
